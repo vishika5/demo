a0bf5d8e801f67a73cfbc2004b7fc680513a4418
vishika5 <133037573+vishika5@users.noreply.github.com>
2024-01-08 12:11:06 +0530

    Update add.py

--- add.py
+++ add.py
@@ -5,9 +5,9 @@ st.header("Types of dogs")
 col1,col2=st.columns(2)
 with col1:
      st.subheader("Pomeranian")
-     st.image("./Pomeranian.jpg",caption="Persian Cat",width=300,use_column_width=True)
+     st.image("./Pomeranian.jpg",caption="Pomeranian",width=300,use_column_width=True)
      st.write("Pomeranian are cute")
 with col2:
       st.subheader("GoldenRetriever")
-      st.image("./GoldenRetriever.jpg",caption="Ragdoll Cat",width=300,use_column_width=True)
+      st.image("./GoldenRetriever.jpg",caption="GoldenRetriever",width=300,use_column_width=True)
       st.write("GoldenRetriever  are cute")
